0c6e99bc6939b740c1ff6e8958b017b75489531f
david-siegert <siegert.D@seznam.cz>
2021-03-12 00:27:26 +0100

    Problem 9 solution

--- problems/problem9.js
+++ problems/problem9.js
@@ -7,3 +7,17 @@
 // There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 // Find the product abc.
 
+for (let a = 0; a < 1000; a++) {
+
+    for (let b = a + 1; b < (1000 - a) / 2; b++) {
+
+        let c = 1000 - (a + b);
+
+        if (a * a + b * b == c * c) {
+
+            console.log(a, b, c, a * b * c);
+
+        }
+
+    }
+}
