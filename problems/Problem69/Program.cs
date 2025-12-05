a791babb7717de61af6a3d39507839823ef3a0a3
David Siegert <siegert.D@seznam.cz>
2025-12-05 17:17:50 +0100

    Problem 69 - trying the GCD approach - still to inefficient

--- problems/Problem69/Program.cs
+++ problems/Problem69/Program.cs
@@ -1,54 +1,14 @@
 ﻿int N = 1000_000;
-List<int> primes = [];
-bool[] isPrimeArray = [];
-Dictionary<int, List<int>> primeFactorsOf = [];
-Dictionary<int, List<int>> relativePrimesOf = [];
-Dictionary<int, int> totients = [];
 int maxN = 0;
-double maxRatio = 0;
+Dictionary<(int, int), int> GCDCache = [];
 
 WrapInStopwatch(() =>
 {
-    primes = GetPrimes(N);
-
-    for (int n = 1; n <= N; n++)
-    {
-        var factors = FindPrimeFactors(n);
-        primeFactorsOf.Add(n, factors);
-    }
-
-    for (int n = 1; n <= N; n++)
-    {
-        if (n % 10000 == 0)
-        {
-            Console.WriteLine($"10000 done");
-        }
-        GetRelativelyPrime(n);
-    }
-
-    for (int n = 1; n <= N; n++)
-    {
-        int phi = relativePrimesOf[n].Count;
-        totients.Add(n, phi);
-    }
-
-    for (int n = 2; n <= N; n++)
-    {
-        double ratio = n / totients[n];
-
-        if (ratio > maxRatio)
-        {
-            maxRatio = ratio;
-            maxN = n;
-        }
-    }
+    maxN = Enumerable.Range(2, N - 1).MaxBy(n => n / (double)Totient(n));
 });
 
-OutputPrimes();
-OutputPrimeFactors();
-// OutputRelativelyPrime();
-//OutputTotients();
 OutputMaximum();
+
 
 void WrapInStopwatch(Action action)
 {
@@ -57,112 +17,48 @@ void WrapInStopwatch(Action action)
     stopwatch.Stop();
     Console.WriteLine($"Elapsed time: {stopwatch.ElapsedMilliseconds} ms");
 }
-void OutputPrimeFactors()
-{
-    Console.WriteLine($"Prime factors of {N}: {string.Join(", ", primeFactorsOf[N])} ");
-}
-void OutputPrimes()
-{
-    Console.WriteLine($"{primes.Count} primes under {N}.");
-    //Console.WriteLine($"{primes.Count} primes under {N}: {string.Join(", ", primes)}");
-}
-void OutputRelativelyPrime()
-{
-    for (int n = 1; n <= N; n++)
-        Console.WriteLine($"Relatively prime numbers to {n}: {string.Join(", ", relativePrimesOf[n])} ");
-}
-void OutputTotients()
-{
-    for (int n = 1; n <= N; n++)
-        Console.WriteLine($"φ({n}) = {totients[n]}");
-}
 void OutputMaximum()
 {
-    Console.WriteLine($"For n = {maxN}, the value of n/φ(n) = {maxRatio} is maximum for n <= {N}");
+    Console.WriteLine($"For n <= {N}, n = {maxN} has the maximum value n/φ(n) = {maxN / (double)Totient(maxN)}");
 }
 
-List<int> GetPrimes(int max)
+void CacheGCD(int a, int b, int value)
 {
-    // initialize bool[] [false, false, true, true, true,..., true]
-    bool[] isPrime = new bool[max + 1];
-    for (int i = 2; i <= max; i++)
+    for (int i = 1; i * a <= N && i * b <= N; i++)
     {
-        isPrime[i] = true;
+        GCDCache[(a * i, b * i)] = value * i;
+        GCDCache[(b * i, a * i)] = value * i;
     }
 
-    int p = 1;
-    while (++p <= Math.Sqrt(max))
-    {
-        if (!isPrime[p]) continue;
-
-        for (int i = p; p * i <= max; i++)
-        {
-            isPrime[p * i] = false;
-        }
-    }
-
-    primes = [];
-    for (int i = 0; i <= max; i++)
-    {
-        if (isPrime[i])
-            primes.Add(i);
-    }
-
-    isPrimeArray = isPrime;
-    return primes;
 }
-List<int> FindPrimeFactors(int number)
+int GCD(int a, int b)
 {
-    List<int> factors = [];
+    if (GCDCache.TryGetValue((a, b), out int cachedGCD2))
+        return cachedGCD2;
 
-    foreach (int p in primes)
-    {
-        int product = 1;
-        foreach (int factor in factors)
-        {
-            product *= factor;
-        }
+    int originalA = a;
+    int originalB = b;
 
-        if (product * p > number)
-            break;
+    while (b != 0)
+        (a, b) = (b, a % b);
 
-        if (number % p == 0)
-        {
-            factors.Add(p);
-        }
-    }
+    CacheGCD(originalA, originalB, a);
+    return a;
 
-    return factors;
 }
-bool ShareFactors(int bigger, int smaller)
+int Totient(int n)
 {
-    var factorsBigger = primeFactorsOf[bigger];
-    var factorsSmaller = primeFactorsOf[smaller];
-
-    foreach (var factor in factorsSmaller)
-    {
-        for (int i = 0; i < factorsBigger.Count /**&& factor <= factorsBigger[i]**/; i++)
-        {
-            if (factor == factorsBigger[i])
-                return true;
-        }
-    }
-
-    return false;
-}
-void GetRelativelyPrime(int n)
-{
-    List<int> relatives = [];
-
-    bool isPrime = isPrimeArray[n];
+    int count = 0;
 
     for (int i = 1; i < n; i++)
     {
-        if (isPrime || i == 1 || !ShareFactors(n, i))
+        if (GCD(n, i) == 1)
         {
-            relatives.Add(i);
+            count++;
         }
     }
 
-    relativePrimesOf.Add(n, relatives);
+    return count;
 }
+
+
