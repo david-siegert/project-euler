d6528ae3c01b4e3b075478e4c482a69f4fe6ec2c
David Siegert <siegert.D@seznam.cz>
2025-12-10 13:06:40 +0100

    Problem 74 - correct solution but slow - 6000ms

---
+++ problems/Problem74/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Diagnostics;
+
+Stopwatch sw = Stopwatch.StartNew();
+
+int[] factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
+
+
+int count = 0;
+for (int start = 1; start < 1000_000; start++)
+{
+    HashSet<int> seen = [];
+    int current = start;
+
+    while (!seen.Contains(current))
+    {
+        seen.Add(current);
+        current = Next(current);
+    }
+
+    if (seen.Count == 60) count++;
+}
+
+sw.Stop();
+Console.WriteLine($"Result: {count} in {sw.ElapsedMilliseconds} ms");
+
+
+int Next(int n)
+{
+    int next = 0;
+
+    n.ToString().Select(ch => ch - '0').ToList().ForEach(digit => next += factorial[digit]);
+
+    return next;
+}
