06db22c6c6f89a8342895439554f70d4c7a65a8b
David Siegert <siegert.D@seznam.cz>
2025-12-12 19:03:21 +0100

    Problem 74 - order of magnitude faster

--- problems/Problem74/Program.cs
+++ problems/Problem74/Program.cs
@@ -1,27 +1,63 @@
 ﻿using System.Diagnostics;
-
 Stopwatch sw = Stopwatch.StartNew();
 
 int[] factorial = [1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880];
 
 
 int count = 0;
+
+Dictionary<int, int> distanceToRepeat = new();
+List<int> chain = [];
+bool startNumSolved = false;
 for (int start = 1; start < 1000_000; start++)
 {
-    HashSet<int> seen = [];
+    chain.Clear();
     int current = start;
+    int indexOfCurrent = -1;
 
-    while (!seen.Contains(current))
+    while (indexOfCurrent == -1)
     {
-        seen.Add(current);
+        // shortcut
+        if (distanceToRepeat.TryGetValue(current, out int dist))
+        {
+            for (int c = 0; c < chain.Count; c++)
+            {
+                distanceToRepeat.Add(chain[c], (chain.Count - c) + dist);
+            }
+
+            startNumSolved = true;
+            break;
+        }
+
+        chain.Add(current);
         current = Next(current);
+        indexOfCurrent = chain.IndexOf(current);
     }
 
-    if (seen.Count == 60) count++;
+    if (startNumSolved) continue;
+
+    // save new loop distances
+    int loopLenght = chain.Count - indexOfCurrent;
+    for (int c = 0; c < chain.Count; c++)
+    {
+        if (c >= indexOfCurrent)
+        {
+            distanceToRepeat.Add(chain[c], loopLenght); // constant for each member of the loop
+        }
+        else
+        {
+            distanceToRepeat.Add(chain[c], chain.Count - c);
+        }
+
+    }
+
 }
 
+
+count = distanceToRepeat.Count(m => m.Value == 60);
+
 sw.Stop();
-Console.WriteLine($"Result: {count} in {sw.ElapsedMilliseconds} ms");
+Console.WriteLine($"{(count == 402 ? "Correct" : "Incorrect")} result: {count} in {sw.ElapsedMilliseconds} ms");
 
 
 int Next(int n)
